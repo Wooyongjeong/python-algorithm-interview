4175e07fce3927bdf7887bf7acdd7239e934f406
wooyongjeong <wooyong.dev@gmail.com>
2021-08-23 21:11:25 +0900

    08 연결 리스트

---
+++ 08 연결 리스트/13 팰린드롬 연결 리스트.py
@@ -0,0 +1,64 @@
+import collections
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# 풀이 1. 리스트 변환
+def isPalindrome(head: ListNode) -> bool:
+    q: list = []
+
+    if not head:
+        return True
+
+    node = head
+    # 리스트 변환
+    while node is not None:
+        q.append(node.val)
+        node = node.next
+
+    # 팰린드롬 판별
+    while len(q) > 1:
+        if q.pop(0) != q.pop():
+            return False
+
+    return True
+
+
+# 풀이 2. 데크를 이용한 최적화
+def isPalindrome2(head: ListNode) -> bool:
+    # 데크 자료형 선언
+    q: collections.deque = collections.deque()
+
+    if not head:
+        return True
+
+    node = head
+    while node is not None:
+        q.append(node.val)
+        node = node.next
+
+    while len(q) > 1:
+        if q.popleft() != q.pop():
+            return False
+
+    return True
+
+# 풀이 3은 Go를 이용한 거였음
+# 풀이 4. 런너(Runner)를 이용한 우아한 풀이
+def isPalindrome4(head: ListNode) -> bool:
+    rev = None
+    slow = fast = head
+    # Runner를 이용해 역순 연결 리스트 구성
+    while fast and fast.next:
+        fast = fast.next.next
+        rev, rev.next, slow = slow, rev, slow.next
+    if fast:
+        slow = slow.next
+
+    # 팰린드롬 여부 확인
+    while rev and rev.val == slow.val:
+        slow, rev = slow.next, rev.next
+    return not rev
---
+++ 08 연결 리스트/14 두 정렬 리스트의 병합.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# 풀이 1. 재귀 구조로 연결
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
