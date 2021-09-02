27a88976ad7a03af60a9c6e05a5ec4f21c77f470
wooyongjeong <wooyong.dev@gmail.com>
2021-09-02 13:24:37 +0900

    [16 트라이] 완료

---
+++ 16 트라이/56 트라이 구현.py
@@ -0,0 +1,52 @@
+import collections
+
+# 풀이 1. 딕셔너리를 이용해 간결한 트라이 구현
+# 트라이의 노드
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+
+class Trie:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    # 단어 삽입
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    # 단어 존재 여부 판별
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return node.word
+
+    # 문자열로 시작 단어 존재 여부 판별별
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return True
